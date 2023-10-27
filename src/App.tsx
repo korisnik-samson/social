945a5c255f5b6e0cb3fab61c88cbb027f637e968
Samson Offorjindu <126519078+korisnik-samson@users.noreply.github.com>
2023-10-27 16:24:48 +0200

    Initial Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import './globals.css';
+
 const App = () => {
     return (
-        <div>App</div>
+        <React.Fragment>
+            <div className="text-3xl font-bold underline">
+                Hello ModernThreads
+            </div>
+        </React.Fragment>
     );
 }
 

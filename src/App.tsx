3f030e8ee5c4c89c25f9d14ab45229e655e323a9
Samson Offorjindu <126519078+korisnik-samson@users.noreply.github.com>
2023-10-27 17:36:45 +0200

    Added Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import { Route, Routes } from "react-router-dom";
+
+import SignInForm from "./_auth/forms/SignInForm.tsx";
+import SignUpForm from "./_auth/forms/SignUpForm.tsx";
+import { Home } from "./_root/pages";
 
 import './globals.css';
+import AuthLayout from "./_auth/AuthLayout.tsx";
+import RootLayout from "./_root/RootLayout.tsx";
 
 const App = () => {
     return (
-        <React.Fragment>
-            <div className="text-3xl font-bold underline">
-                Hello ModernThreads
-            </div>
-        </React.Fragment>
+        <main className="flex h-screen">
+            <Routes>
+                {/* public routes */}
+                <Route element={<AuthLayout />}>
+                    <Route path='/sign-in' element={<SignInForm />} />
+                    <Route path='/sign-up' element={<SignUpForm />} />
+                </Route>
+
+                {/* private routes */}
+                <Route element={<RootLayout />}>
+                    <Route index element={<Home />} />
+                </Route>
+            </Routes>
+        </main>
     );
 }
 
